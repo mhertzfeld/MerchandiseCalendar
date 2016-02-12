e3e5ff4cbe9830e2f08f3ac3897020b4298bf5bb
Matthew Hertzfeld <matt.hertzfeld@gmail.com>
2016-02-12 11:08:07 -0500

    Signed-off-by: Matthew Hertzfeld <matt.hertzfeld@gmail.com>

--- MerchandiseDate.cs
+++ MerchandiseDate.cs
@@ -4,40 +4,148 @@ namespace MerchandiseCalendar
 {
     public class MerchandiseDate
     {
-        public DateTime? Date;
+        //FIELDS
+        DateTime? date;
 
-        public Int32? DayOfYear;
+        Int32 dayOfYear;
 
-        public Int32? Period;
+        Int32 period;
 
-        public String PeriodName;
+        String periodName;
 
-        public Int32? Quarter;
+        Int32 quarter;
 
-        public String QuarterName;
+        String quarterName;
 
-        public Int32? Week;
+        Int32 week;
 
-        public Int32? Year;
+        Int32 year;
 
 
+        //PROPERTIES
+        public DateTime? Date
+        {
+            get { return date; }
+
+            set
+            {
+                if (value == default(DateTime?))
+                { throw new Exception("Date cannot be set to null."); }
+
+                date = value;
+            }
+        }
+
+        public Int32 DayOfYear
+        {
+            get { return dayOfYear; }
+
+            set
+            {
+                if ((value < 1) || (value > 371))
+                { throw new Exception("DayOfYear cannot be less than 1 or greater than 371."); }
+
+                dayOfYear = value;
+            }
+        }
+
+        public Int32 Period
+        {
+            get { return period; }
+
+            set
+            {
+                if ((value < 1) || (value > 12))
+                { throw new Exception("Period cannot be less than 1 or greater than 12."); }
+
+                period = value;
+            }
+        }
+
+        public String PeriodName
+        {
+            get { return periodName; }
+
+            set
+            {
+                if (value == default(String))
+                { throw new Exception("PeriodName cannot be set to null."); }
+
+                periodName = value;
+            }
+        }
+
+        public Int32 Quarter
+        {
+            get { return quarter; }
+
+            set
+            {
+                if ((value < 1) || (value > 4))
+                { throw new Exception("Quarter cannot be less than 1 or greater than 4."); }
+
+                quarter = value;
+            }
+        }
+
+        public String QuarterName
+        {
+            get { return quarterName; }
+
+            set
+            {
+                if (value == default(String))
+                { throw new Exception("quarterName cannot be set to null."); }
+
+                quarterName = value;
+            }
+        }
+
+        public Int32 Week
+        {
+            get { return week; }
+
+            set
+            {
+                if ((value < 1) || (value > 53))
+                { throw new Exception("Week cannot be less than 1 or greater than 53."); }
+
+                week = value;
+            }
+        }
+
+        public Int32 Year
+        {
+            get { return year; }
+
+            set
+            {
+                if ((value < 1) || (value > 9999))
+                { throw new Exception("Week cannot be less than 1 or greater than 9999."); }
+
+                year = value;
+            }
+        }
+
+
+        //INITIALIZE
         public MerchandiseDate()
         {
-            Date = null;
+            date = null;
 
-            DayOfYear = null;
+            dayOfYear = 0;
 
-            Period = null;
+            period = 0;
 
-            PeriodName = null;
+            periodName = null;
 
-            Quarter = null;
+            quarter = 0;
 
-            QuarterName = null;
+            quarterName = null;
 
-            Week = null;
+            week = 0;
 
-            Year = null;
+            year = 0;
         }
     }
 }
