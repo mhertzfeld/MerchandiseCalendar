efa67acdf917ccdd0a9f87a9276e3af6771cc1be
Matthew Hertzfeld <matthew.hertzfeld@protonmail.com>
2017-01-05 15:08:35 -0500

    Signed-off-by: Matthew Hertzfeld <matthew.hertzfeld@protonmail.com>
    New Branch.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MerchandiseCalendar")]
-[assembly: AssemblyCopyright("Copyright (c) 2016 Dromaeosaur - The MIT License (MIT)")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 Dromaeosaur - The MIT License (MIT) ***Forked by Matthew.Hertzfeld@ProtonMail.com")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
